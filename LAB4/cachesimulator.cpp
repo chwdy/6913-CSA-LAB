b8b756e055b3519990ce6fa6dd32b2c4702022ce
David Zhu <1059126322@qq.com>
2020-11-30 17:40:53 -0500

    debug setup

--- LAB4/cachesimulator.cpp
+++ LAB4/cachesimulator.cpp
@@ -22,127 +22,101 @@ using namespace std;
 #define WH 3 // Write hit
 #define WM 4 // write miss
 
-
-
-
-struct config{
-       int L1blocksize;
-       int L1setsize;
-       int L1size;
-       int L2blocksize;
-       int L2setsize;
-       int L2size;
-       };
+struct config
+{
+    int L1blocksize;
+    int L1setsize;
+    int L1size;
+    int L2blocksize;
+    int L2setsize;
+    int L2size;
+};
 
 /* you can define the cache class here, or design your own data structure for L1 and L2 cache
 class cache {
       
       }
-*/       
+*/
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 
-
-    
     config cacheconfig;
     ifstream cache_params;
     string dummyLine;
     cache_params.open(argv[1]);
-    while(!cache_params.eof())  // read config file
+    while (!cache_params.eof()) // read config file
     {
-      cache_params>>dummyLine;
-      cache_params>>cacheconfig.L1blocksize;
-      cache_params>>cacheconfig.L1setsize;              
-      cache_params>>cacheconfig.L1size;
-      cache_params>>dummyLine;              
-      cache_params>>cacheconfig.L2blocksize;           
-      cache_params>>cacheconfig.L2setsize;        
-      cache_params>>cacheconfig.L2size;
-      }
-    
-  
-  
-   // Implement by you: 
-   // initialize the hirearch cache system with those configs
-   // probably you may define a Cache class for L1 and L2, or any data structure you like
-   
-   
-   
-   
-  int L1AcceState =0; // L1 access state variable, can be one of NA, RH, RM, WH, WM;
-  int L2AcceState =0; // L2 access state variable, can be one of NA, RH, RM, WH, WM;
-   
-   
+        cache_params >> dummyLine;
+        cache_params >> cacheconfig.L1blocksize;
+        cache_params >> cacheconfig.L1setsize;
+        cache_params >> cacheconfig.L1size;
+        cache_params >> dummyLine;
+        cache_params >> cacheconfig.L2blocksize;
+        cache_params >> cacheconfig.L2setsize;
+        cache_params >> cacheconfig.L2size;
+    }
+
+    // Implement by you:
+    // initialize the hirearch cache system with those configs
+    // probably you may define a Cache class for L1 and L2, or any data structure you like
+
+    int L1AcceState = 0; // L1 access state variable, can be one of NA, RH, RM, WH, WM;
+    int L2AcceState = 0; // L2 access state variable, can be one of NA, RH, RM, WH, WM;
+
     ifstream traces;
     ofstream tracesout;
     string outname;
     outname = string(argv[2]) + ".out";
-    
+
     traces.open(argv[2]);
     tracesout.open(outname.c_str());
-    
+
     string line;
-    string accesstype;  // the Read/Write access type from the memory trace;
-    string xaddr;       // the address from the memory trace store in hex;
-    unsigned int addr;  // the address from the memory trace store in unsigned int;        
+    string accesstype;     // the Read/Write access type from the memory trace;
+    string xaddr;          // the address from the memory trace store in hex;
+    unsigned int addr;     // the address from the memory trace store in unsigned int;
     bitset<32> accessaddr; // the address from the memory trace store in the bitset;
-    
-    if (traces.is_open()&&tracesout.is_open()){    
-        while (getline (traces,line)){   // read mem access file and access Cache
-            
-            istringstream iss(line); 
-            if (!(iss >> accesstype >> xaddr)) {break;}
+
+    if (traces.is_open() && tracesout.is_open())
+    {
+        while (getline(traces, line))
+        { // read mem access file and access Cache
+
+            istringstream iss(line);
+            if (!(iss >> accesstype >> xaddr))
+            {
+                break;
+            }
             stringstream saddr(xaddr);
             saddr >> std::hex >> addr;
-            accessaddr = bitset<32> (addr);
-           
-           
-           // access the L1 and L2 Cache according to the trace;
-              if (accesstype.compare("R")==0)
-              
-             {    
-                 //Implement by you:
-                 // read access to the L1 Cache, 
-                 //  and then L2 (if required), 
-                 //  update the L1 and L2 access state variable;
-                 
-                 
-                 
-                 
-                 
-                 
-                 
-                 }
-             else 
-             {    
-                   //Implement by you:
-                  // write access to the L1 Cache, 
-                  //and then L2 (if required), 
-                  //update the L1 and L2 access state variable;
-                  
-                  
-                  
-                  
-                  
-                  
-                  }
-              
-              
-             
-            tracesout<< L1AcceState << " " << L2AcceState << endl;  // Output hit/miss results for L1 and L2 to the output file;
-             
-             
+            accessaddr = bitset<32>(addr);
+
+            cout << accesstype << "    " << accessaddr << endl;
+            // access the L1 and L2 Cache according to the trace;
+            if (accesstype.compare("R") == 0)
+
+            {
+                //Implement by you:
+                // read access to the L1 Cache,
+                //  and then L2 (if required),
+                //  update the L1 and L2 access state variable;
+            }
+            else
+            {
+                //Implement by you:
+                // write access to the L1 Cache,
+                //and then L2 (if required),
+                //update the L1 and L2 access state variable;
+            }
+
+            tracesout << L1AcceState << " " << L2AcceState << endl; // Output hit/miss results for L1 and L2 to the output file;
         }
         traces.close();
-        tracesout.close(); 
+        tracesout.close();
     }
-    else cout<< "Unable to open trace or traceout file ";
+    else
+        cout << "Unable to open trace or traceout file ";
 
-
-   
-    
-  
-
-   
     return 0;
 }
